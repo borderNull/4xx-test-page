073cfcc5764eb977155dcf35f50a94946e1f5918
borderNull <spbtvtower@gmail.com>
2016-12-08 21:06:50 +0300

    fix

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 * клонировать репозиторий
 * перейти в папку с проектом
+* npm install gulpjs/gulp-cli#4.0 -g
 * npm install 
 * run gulp 
 * открыть localhost:3000
--- source/js/main.js
+++ source/js/main.js
@@ -6,14 +6,15 @@ window.addEventListener('resize', function() {
     checkWidth();
 });
 
-let addBtn = document.querySelector('.info__btn'),
-    shareBtn = document.querySelector('.btn__share');
+// let addBtn = document.querySelector('.info__btn'),
+//     offerSection = document.querySelector('.offer__wrap'),
+
+let shareBtn = document.querySelector('.btn__share');
 
 document.addEventListener('click', function(e) {
-    let shareIcon = document.querySelectorAll('.timestamp__svg.share'),
-        pageNote = document.querySelectorAll('.page__note'),
-        offerSection = document.querySelector('.offer__wrap'),
-        pageWrap = document.querySelector('.page__wrap');
+    let shareIcon = document.querySelectorAll('.timestamp__svg.share');
+    // pageNote = document.querySelectorAll('.page__note'),
+    // pageWrap = document.querySelector('.page__wrap');
 
     if (e.target == shareBtn) {
         modalWindow();
@@ -25,36 +26,37 @@ document.addEventListener('click', function(e) {
         }
     }
 
-    for (let v = 0; v < pageNote.length; v++) {
-        Array.prototype.filter.call(e.target.parentNode.children, function(child) {
-            if (e.target == pageWrap) {
+    // for (let v = 0; v < pageNote.length; v++) {
+    //     Array.prototype.filter.call(e.target.parentNode.children, function(child) {
+    //         if (e.target == pageWrap) {
 
-                pageNote[v].style.display = 'block';
-                shareBtn.style.display = 'block';
-                offerSection.style.display = 'block';
-                addBtn.style.width = '140px';
-                pageWrap.style.height = '';
-            }
+    //             pageNote[v].style.display = 'block';
+    //             shareBtn.style.display = 'block';
+    //             offerSection.style.display = 'block';
+    //             // addBtn.style.width = '140px';
+    //             pageWrap.style.height = '';
+    //         }
 
-            if (e.target == pageNote[v]) {
-                offerSection.style.display = 'none';
-                shareBtn.style.display = 'none';
-                addBtn.style.width = '100%';
-                pageWrap.style.minHeight = '70vh';
-                if (child !== e.target) {
-                    child.style.display = 'none';
+    //         if (e.target == pageNote[v]) {
+    //             offerSection.style.display = 'none';
+    //             shareBtn.style.display = 'none';
+    //             addBtn.style.width = '100%';
+    //             pageWrap.style.minHeight = '80vh';
+    //             if (child !== e.target) {
+    //                 child.style.display = 'none';
 
-                }
-            }
-        });
-    }
+    //             }
+    //         }
+    //     });
+    // }
+
 });
 
 
 function checkWidth() {
     let logoDesc = document.querySelector('.logo__description'),
         screenWidth = window.innerWidth,
-        signupLink = document.querySelector('.header__link');
+        signupLink = document.querySelector('.header-signup__link');
 
     if (screenWidth < 767) {
         logoDesc.innerText = 'Free on the Google Play';
@@ -63,10 +65,13 @@ function checkWidth() {
     } else if (screenWidth > 767) {
         logoDesc.innerText = 'Download it for free on the App Store';
         signupLink.innerText = 'sign up for free';
-        addBtn.style.width = '140px';
+        // addBtn.style.width = '140px';
         shareBtn.style.display = 'none';
 
-    } 
+    }
+    // else if (screenWidth > 1199) {
+    //    offerSection.style.display = 'none';
+    // }
 }
 
 function modalWindow() {
